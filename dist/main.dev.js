9a756b96f75cace50ff35ba2c45e0cb13f1fb4ec
Art <aivaneftz@gmail.com>
2023-04-10 14:51:27 +0200

    scripts

---
+++ dist/main.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+VANTA.GLOBE({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+});
